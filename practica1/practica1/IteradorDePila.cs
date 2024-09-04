c6d19905ff8a889a91eea2ef0aba7ce2dc4d893e
Denise Gonzalez <nissgonzalezm@gmail.com>
2024-09-04 19:37:18 -0300

    Add files via upload

---
+++ practica1/practica1/IteradorDePila.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Almnos
+ * Date: 4/9/2024
+ * Time: 19:00
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica1
+{
+	/// <summary>
+	/// Description of IteradorDePila.
+	/// </summary>
+	public class IteradorDePila:Iterador
+	{
+		private Pila p;
+		private int indice;
+		
+		public IteradorDePila(Pila p )
+		{
+			this.p = p;
+			primero;
+		}
+		
+		public void primero (){
+			this.indice = 0;
+		}
+		public void siguiente(){
+			this.indice++;
+		}
+		public bool fin(){
+			return this.indice >= p.cuantos();
+		}
+		public Comparable actual(){
+			return p.getElementos()[indice];
+		}
+	}
+}
---
+++ practica1/practica1/Program.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Almnos
+ * Date: 4/9/2024
+ * Time: 17:56
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica1
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			
+			
+			
+	
+			
+			Console.ReadKey(true);
+		}
+		
+		// cliente no cambia 
+		public static void imprimirElementos(Coleccionable col){
+			//suponiendo que todos los coleccionables son iterables y conocen su iterador por l que es un interfaz
+			//creo iterador
+			Iterador ite = col.crearIterador();
+			ite.primero();
+			while (!ite.fin){
+				Console.WriteLine(ite.actual()); //set estrategia en el ejercicio 9
+				ite.siguiente();
+			}
+			
+		}
+	}
+	
+}
